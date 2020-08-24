3e0644b8b403f6dbf5c23b25773fc636fed5e054
Vinicius Ribeiro <56563464+vribeirocmp@users.noreply.github.com>
2020-08-24 14:39:43 -0300

    Scritp to send mail via Power Shell

---
+++ send_mail.ps1
@@ -0,0 +1,11 @@
+﻿
+
+$EmailFrom = "email"
+$EmailTo = "email"
+$Subject = "Subject"
+$Body = "Body message"
+$SMTPServer = "smtp server"
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential(user,password);
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
